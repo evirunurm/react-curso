b595ba266ec2cd10c94dd9c02d67bb38a13eaf9a
Francho Joven <fjoven@plainconcepts.com>
2024-06-09 23:41:35 +0200

    feat: el search coge el foco automáticamente

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,13 +1,21 @@
-import { FC } from 'react'
+import { FC, useEffect, useRef } from 'react'
 
 export interface SearchBarProps {
   onSearch: (value: string) => void
 }
 
-const SearchBar: FC<SearchBarProps> = ({ onSearch: onSearchChange }) => (
-  <div>
-    <input type="text" placeholder="buscar" onChange={(e) => onSearchChange(e.target.value)} />
-  </div>
-)
+const SearchBar: FC<SearchBarProps> = ({ onSearch: onSearchChange }) => {
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    inputRef.current?.focus()
+  }, [])
+
+  return (
+    <div>
+      <input ref={inputRef} type="text" placeholder="buscar" onChange={(e) => onSearchChange(e.target.value)} />
+    </div>
+  )
+}
 
 export default SearchBar
