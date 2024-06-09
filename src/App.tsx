9fcf6e6a6150580e8f735e5303b04e674cff14db
Francho Joven <fjoven@plainconcepts.com>
2024-06-09 21:43:31 +0200

    feat: dibuja lista de tareas

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,32 @@ import TaskCard from './components/TaskCard/TaskCard'
 import { Task } from './types'
 
 function App() {
-  const todo: Task = {
-    id: 1,
-    title: 'Hacer la compra',
-    description: 'Comprar pan, leche y huevos',
-    completed: false,
-  }
+  const todoList: Task[] = [
+    {
+      id: 1,
+      title: 'Hacer la compra',
+      description: 'Comprar pan, leche y huevos',
+      completed: false,
+    },
+    {
+      id: 2,
+      title: 'Ir al gimnasio',
+      description: 'Hacer una hora de pesas',
+      completed: true,
+    },
+    {
+      id: 3,
+      title: 'Estudiar inglés',
+      description: 'Repasar los verbos irregulares',
+      completed: false,
+    },
+  ]
 
   return (
     <Layout>
-      <TaskCard task={todo} />
+      {todoList.map((todo) => (
+        <TaskCard task={todo} key={`task-${todo.id}`} />
+      ))}
       <div>Contenido de la aplicación</div>
     </Layout>
   )
